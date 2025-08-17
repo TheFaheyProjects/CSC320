8c354bb2f26ba940d1b53027cc332b7062c08b1a
Kyle Fahey <kyle@Kyles-MacBook-Pro.local>
2025-08-17 13:58:46 -0600

    Adding Critical Thinking Module One

---
+++ Critical Thinking: Module 1/CSC320_Module1.java
@@ -0,0 +1,28 @@
+// Kyle Fahey
+// CSC320-1
+// Critical Thinking: Modolue 1
+
+// Option 1: List Individuals Name and Address Information
+
+import java.util.Scanner;
+
+public class CSC320_Module1 {
+    
+    public static void main(String[] args) {
+
+        // Declaring the Variables
+        String firstName = "Levi";
+        String lastName = "Kitchen";
+        String streetAddress = "40205 NE Borin Rd";
+        String city = "Washougal";
+        String zipcode = "98671";
+
+        // Printing the Varibales on Seperate Lines
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zipcode: " + zipcode);
+        
+    }
+}
